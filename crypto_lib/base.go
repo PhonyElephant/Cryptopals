41bbcaf5f887601bbab454cb281e62ff16a46154
Arif Fil <arif.fil@outlook.com>
2025-06-13 15:30:49 +0300

    add XOR encryption and decryption functions

--- crypto_lib/base.go
+++ crypto_lib/base.go
@@ -16,13 +16,13 @@ func hexToByteValue(hex string) (byte, error) {
 
 }
 
-func HexDecode(s string) ([]byte, error) {
+func HexDecode(hexString string) ([]byte, error) {
 
-	rawBytes := make([]byte, 0, len(s)/2)
+	rawBytes := make([]byte, 0, len(hexString)/2)
 
-	for i := 0; i < len(s); i += 2 {
+	for i := 0; i < len(hexString); i += 2 {
 
-		hexByte := s[i: i+2]
+		hexByte := hexString[i: i+2]
 
 		byteVal, err := hexToByteValue(hexByte)
 
@@ -35,5 +35,36 @@ func HexDecode(s string) ([]byte, error) {
 	}
 
 	return rawBytes, nil
+
+}
+
+func HexEncode(rawBytes []byte) string {
+
+	hexString := ""
+
+	for _, b := range(rawBytes) {
+		hexString += strconv.FormatUint(uint64(b), 16)
+	}
+
+	return hexString
+
+}
+
+func XorEncrypt(plaintext, key []byte) []byte {
+
+	ciphertext := make([]byte, 0, len(plaintext))
+
+	for i := 0; i < len(plaintext); i++ {
+		ciphertext = append(ciphertext, plaintext[i] ^ key[i])
+	}
+
+	return ciphertext
+
+}
+
+func XorDecrypt(ciphertext, key []byte) []byte {
+
+	return XorEncrypt(ciphertext, key)
+
 
 }
